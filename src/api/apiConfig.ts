52d293f0b725f39c8652243fd286c4303df6562f
Fabiano Braga <fabianobragadev@gmail.com>
2025-04-22 15:57:07 -0300

    feat: Remover Comentário - API e Serviço

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://192.168.86.12:3333/',
   headers: {
     Authorization:
-      'Bearer NQ.8354nd2js9qgXIeMBsOqvUb5zAGGQkqS5ILm24nBl2ObTPm_CUlgw79kDqzN',
+      'Bearer Ng.Gf5W5XPBfgCN1WEJsCrtP3tL7GUAGj-Zgj9U0hy8R7n-mtic9VMmbpF5eNA5',
   },
 });
