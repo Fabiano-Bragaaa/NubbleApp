05e0d6a417b488412d6859e141d498f00c842b16
Fabiano Braga <fabianobragadev@gmail.com>
2025-04-23 12:19:17 -0300

    feat: Toast Component

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.86.12:3333/',
+  baseURL: 'http://192.168.86.4:3333/',
   headers: {
     Authorization:
-      'Bearer Ng.Gf5W5XPBfgCN1WEJsCrtP3tL7GUAGj-Zgj9U0hy8R7n-mtic9VMmbpF5eNA5',
+      'Bearer Nw.ciTokF5eWLbQpJzc-DSdeZghMxIqyVOkafdDGdRW-HWWebMNd2IzmvPaZ7qB',
   },
 });
