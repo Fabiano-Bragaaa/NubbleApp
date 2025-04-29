3e57ff17295ef03eafa9a3a7fccb0b3cd241ceb6
Fabiano Braga <fabianobragadev@gmail.com>
2025-04-29 15:02:53 -0300

    feat: add profile screen

---
+++ src/screens/app/Profile/Profile.tsx
@@ -0,0 +1,30 @@
+import {useUserGetById} from '@domain';
+
+import {ActivityIndicator, Box, ProfileAvatar, Screen, Text} from '@components';
+import {AppScreenProps} from '@routes';
+
+export function Profile({route}: AppScreenProps<'Profile'>) {
+  const userId = route.params.userId;
+
+  const {error, loading, user} = useUserGetById(userId);
+
+  return (
+    <Screen canGoBack>
+      {loading && <ActivityIndicator />}
+      {error && <Text>Erro ao carregar o perfil do usúario</Text>}
+      {user && (
+        <Box alignItems="center">
+          <ProfileAvatar
+            imageUrl={user.profileUrl}
+            size={64}
+            borderRadius={24}
+          />
+          <Text preset="headingMedium" bold>
+            {user.fullName}
+          </Text>
+          <Text>@{user.username}</Text>
+        </Box>
+      )}
+    </Screen>
+  );
+}
