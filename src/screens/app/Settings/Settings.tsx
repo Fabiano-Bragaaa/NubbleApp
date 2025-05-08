7db32e43e73e7c40f378e6c555f300192f4682a4
Fabiano Braga <fabianobragadev@gmail.com>
2025-05-08 15:58:07 -0300

    feat: add settings screen

--- src/screens/app/Settings/Settings.tsx
+++ src/screens/app/Settings/Settings.tsx
@@ -1,10 +1,17 @@
-import {Screen, Text} from '@components';
+import {useAuthSignOut} from '@domain';
+
+import {Button, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
 export function Settings({navigation}: AppScreenProps<'Settings'>) {
+  const {isLoading, signOut} = useAuthSignOut();
+
   return (
-    <Screen canGoBack>
-      <Text preset="headingLarge">Settings</Text>
+    <Screen canGoBack title="Configurações">
+      <Text preset="headingLarge" mb="s10">
+        Settings
+      </Text>
+      <Button title="Sair da conta" loading={isLoading} onPress={signOut} />
     </Screen>
   );
 }
