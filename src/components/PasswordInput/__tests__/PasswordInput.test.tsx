0a9a7e1ce56c1e6b54a3b6c9bc9e17976fdfa1d8
Fabiano Braga <fabianobragadev@gmail.com>
2025-05-28 22:51:50 -0300

    Testando da Perspectiva do Usuário

--- src/components/PasswordInput/__tests__/PasswordInput.test.tsx
+++ src/components/PasswordInput/__tests__/PasswordInput.test.tsx
@@ -1,6 +1,6 @@
-import {render, screen} from 'test-utils';
+import {fireEvent, render, screen} from 'test-utils';
 
-import {PasswordInput} from '../PasswordInput';
+import {PasswordInput, IconProps} from '@components';
 
 describe('<PasswordInput />', () => {
   test('start with hidden password', () => {
@@ -17,5 +17,29 @@ describe('<PasswordInput />', () => {
 
     const inputElement = screen.getByPlaceholderText(/password/i);
     expect(inputElement.props.secureTextEntry).toBeTruthy();
+  });
+
+  test('when pressing the eye icon, it should show the password and change to the eye off icon', () => {
+    const mockedOnChange = jest.fn();
+
+    render(
+      <PasswordInput
+        label="Password"
+        placeholder="Password"
+        value="123456"
+        onChangeText={mockedOnChange}
+      />,
+    );
+
+    const eyeIcon: IconProps['name'] = 'eyeOn';
+    fireEvent.press(screen.getByTestId(eyeIcon));
+
+    const eyeOffIcon: IconProps['name'] = 'eyeOff';
+    const eyeOffIconElement = screen.getByTestId(eyeOffIcon);
+
+    const inputElement = screen.getByPlaceholderText(/password/i);
+
+    expect(eyeOffIconElement).toBeTruthy();
+    expect(inputElement.props.secureTextEntry).toBeFalsy();
   });
 });
