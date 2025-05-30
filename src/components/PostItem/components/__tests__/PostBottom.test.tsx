5bfd1b014d0881c2c9b4e20b7e5372e92b4e7304
Fabiano Braga <fabianobragadev@gmail.com>
2025-05-29 23:17:56 -0300

    Mock Function (useNavigation)

--- src/components/PostItem/components/__tests__/PostBottom.test.tsx
+++ src/components/PostItem/components/__tests__/PostBottom.test.tsx
@@ -1,5 +1,5 @@
 import {Post} from '@domain';
-import {render, screen} from 'test-utils';
+import {fireEvent, render, screen} from 'test-utils';
 
 import {PostBottom} from '../PostBottom';
 
@@ -17,12 +17,44 @@ const mockedPost: Post = {
     userName: 'fabiano',
   },
 };
+const mockedNavigate = jest.fn();
+jest.mock('@react-navigation/native', () => {
+  const originalModule = jest.requireActual('@react-navigation/native');
+  return {
+    ...originalModule,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
 describe('<PostBottom />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   test('it does not show the comment link if it has no comment', () => {
     render(<PostBottom {...mockedPost} commentCount={0} />);
 
     const commentLinkElement = screen.queryByText(/comentário/);
 
     expect(commentLinkElement).toBeFalsy();
+  });
+  test('navigates to PostComment when pressing the comment link', () => {
+    render(<PostBottom {...mockedPost} commentCount={4} />);
+
+    const commentLinkElement = screen.getByText(/comentário/);
+
+    fireEvent.press(commentLinkElement);
+
+    expect(mockedNavigate).toHaveBeenCalledWith('PostComment', {
+      postId: mockedPost.id,
+      postAuthor: mockedPost.author.id,
+    });
+  });
+  test('it show the comment link if it has a comment', () => {
+    render(<PostBottom {...mockedPost} commentCount={1} />);
+
+    const commentLinkElement = screen.queryByText(/comentário/);
+
+    expect(commentLinkElement).toBeTruthy();
   });
 });
