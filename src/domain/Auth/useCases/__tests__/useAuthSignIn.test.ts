fd92223d42f0ebb0d6ec85d8235da0d6cc83d4f6
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-03 13:01:25 -0300

    Testando Hook e Usando Mocks (1/2)

--- src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
+++ src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
@@ -1,12 +1,20 @@
-import {AllTheProviders, renderHook} from 'test-utils';
+import {AllTheProviders, renderHook, waitFor} from 'test-utils';
 
+import {authService} from '../../authService';
 import {useAuthSignIn} from '../useAuthSignIn';
 
+import { mockedAuthCredentials } from './mockedData/mocks';
+
 describe('useAuthSignIn', () => {
-  it('saves credentials if the sign-in successfully', () => {
+  it('saves credentials if the sign-in successfully', async () => {
+    jest.spyOn(authService, 'signIn').mockResolvedValueOnce(mockedAuthCredentials);
     const {result} = renderHook(() => useAuthSignIn(), {
       wrapper: AllTheProviders,
     });
+
+    result.current.signIn({email: 'lucas@coffstack.com', password: '123'});
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
   });
 
   it('calls the onError function with a message if sign-in fails', () => {
