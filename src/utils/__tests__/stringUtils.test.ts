2a3852aafca73f671d396ad508d8160b9ef0b6c3
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-16 11:24:15 -0300

    Introdução a Mock com spyOn

--- src/utils/__tests__/stringUtils.test.ts
+++ src/utils/__tests__/stringUtils.test.ts
@@ -1,8 +1,18 @@
-import { stringUtils } from '../stringsUtils';
+import {stringUtils} from '../stringsUtils';
 
-test('Caitalize Fist Letter', () => {
+describe('stringUtils', () => {
+  describe('capitalizeFirstLetter', () => {
+    it('should capitalize the first letter of each word', () => {
+      stringUtils.capitalizeFirstLetter('Ana maria');
 
-  stringUtils.capitalizeFirstLetter('Ana maria');
+      expect(stringUtils.capitalizeFirstLetter('Ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('anA MariA')).toBe('Ana Maria');
+    });
 
-  expect(stringUtils.capitalizeFirstLetter('Ana maria')).toBe('Ana Maria');
+    it('should remove leading/trailing spaces', () => {
+      expect(stringUtils.capitalizeFirstLetter(' anA MariA')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('anA MariA ')).toBe('Ana Maria');
+    });
+  });
 });
