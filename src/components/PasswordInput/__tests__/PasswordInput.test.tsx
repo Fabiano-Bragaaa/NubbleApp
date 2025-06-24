20d6a6756966ffb7e8276b06dc599be74d05f70b
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-24 19:27:31 -0300

    Testando propriedades do Componente

---
+++ src/components/PasswordInput/__tests__/PasswordInput.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from 'test-utils';
+
+import { PasswordInput } from '../PasswordInput';
+
+describe('<PasswordInput />', () =>{
+  it('starts with hidden password', () => {
+    const mockedOnChange = jest.fn();
+
+    render(
+      <PasswordInput
+        label="Password"
+        placeholder="password"
+        value="123456"
+        onChangeText={mockedOnChange}
+        />,
+    );
+
+    const inputElement = screen.getByPlaceholderText(/password/i);
+    expect(inputElement.props.secureTextEntry).toBeTruthy();
+  });
+});
