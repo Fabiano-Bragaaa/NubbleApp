3dd98b1891f4710c06aa5a187cb55b16f98a2f45
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-10-18 09:42:10 -0300

    Teste: Jest Timer e Descrição das Etapas

--- src/screens/app/Search/__tests__/integration/Search.test.tsx
+++ src/screens/app/Search/__tests__/integration/Search.test.tsx
@@ -1,6 +1,6 @@
 import {authCredentialsStorage} from '@services';
 import {mockUtils, server, userMocked} from '@test';
-import {fireEvent, renderScreen, screen} from 'test-utils';
+import {act, fireEvent, renderScreen, screen} from 'test-utils';
 
 import {AppStack} from '@routes';
 
@@ -11,6 +11,7 @@ beforeAll(() => {
   jest
     .spyOn(authCredentialsStorage, 'get')
     .mockResolvedValue(mockUtils.mateusAuthCredentials);
+  jest.useFakeTimers();
 });
 
 afterEach(() => {
@@ -20,25 +21,63 @@ afterEach(() => {
 afterAll(() => {
   server.close();
   jest.resetAllMocks();
+  jest.useRealTimers();
 });
 
 describe('Integration: Search', () => {
   test('Search Flow', async () => {
+    //1) Render the screen
     renderScreen(<AppStack initialRouteName="Search" />);
 
     const inputElement = screen.getByPlaceholderText(/digite sua busca/i);
 
+    //2) Type 'mar' in the input
     fireEvent.changeText(inputElement, 'mar');
+    act(() => jest.runAllTimers());
 
+    //3) Find the first user
     const user1 = await screen.findByText(userMocked.user1.username);
     expect(user1).toBeTruthy();
 
+    //4) Find the second user
     const user2 = screen.getByText(userMocked.user2.username);
     expect(user2).toBeTruthy();
 
+    //5) Press the first user
     fireEvent.press(user1);
 
+    //6) Find the full name of the first user
     const userFullName = await screen.findByText(userMocked.user1.full_name);
     expect(userFullName).toBeTruthy();
+
+    //7) Press the back button
+    const backButton = screen.getByText(/voltar/i);
+    fireEvent.press(backButton);
+
+    //7) Find the first user
+    const user1Again = await screen.findByText(userMocked.user1.username);
+    expect(user1Again).toBeTruthy();
+
+    //8) clean the search input
+    fireEvent.changeText(inputElement, '');
+    act(() => jest.runAllTimers());
+
+    //9) Find the search history
+    const searchHistory = await screen.findByText(/buscas recentes/i);
+    expect(searchHistory).toBeTruthy();
+
+    //10) Find the first user (pressed) in the search history
+    const user1InSearchHistory = await screen.findByText(
+      userMocked.user1.username,
+    );
+    expect(user1InSearchHistory).toBeTruthy();
+
+    //11) the user 2 (not pressed) should not be in the search history
+    const user2InSearchHistory = screen.queryByText(userMocked.user2.username);
+    expect(user2InSearchHistory).toBeNull();
+
+    //12) press the trash icon of the first user in the search history
+
+    //13) the first user should not be in the search history
   });
 });
