ebcdf243cd6e0d82b8360a8212b94c561ae3a918
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-10-22 08:25:28 -0300

    Permissão no Android (permissionService)

--- src/services/permission/permissionService.tsx
+++ src/services/permission/permissionService.tsx
@@ -1,12 +1,50 @@
+import {Permission, PermissionsAndroid, Platform} from 'react-native';
+
 import {
   PermissionName,
   PermissionService,
   PermissionStatus,
 } from './permissionTypes';
 
-async function check(name: PermissionName): Promise<PermissionStatus> {}
+async function check(name: PermissionName): Promise<PermissionStatus> {
+  const permission = mapNameToPermission(name);
+  if (permission) {
+    const result = await PermissionsAndroid.check(permission);
+    if (result) {
+      return 'granted';
+    } else {
+      return 'denied';
+    }
+  }
 
-async function request(name: PermissionName): Promise<PermissionStatus> {}
+  return 'unavailable';
+}
+
+async function request(name: PermissionName): Promise<PermissionStatus> {
+  const permission = mapNameToPermission(name);
+  if (permission) {
+    const result = await PermissionsAndroid.request(permission);
+    return result;
+  }
+
+  return 'unavailable';
+}
+
+function mapNameToPermission(name: PermissionName): Permission {
+  switch (name) {
+    case 'photoLibrary':
+      if (Number(Platform.Version) >= 33) {
+        return 'android.permission.READ_MEDIA_IMAGES';
+      } else {
+        return 'android.permission.READ_EXTERNAL_STORAGE';
+      }
+
+    case 'camera':
+      return 'android.permission.CAMERA';
+    default:
+      throw new Error(`Invalid permission name: ${name}`);
+  }
+}
 
 export const permissionService: PermissionService = {
   check,
