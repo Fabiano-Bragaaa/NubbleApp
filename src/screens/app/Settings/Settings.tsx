6b412cb48228f8eea01985cc773bebe0d26243e8
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-11-04 22:21:32 -0300

    Tela de Configurações

--- src/screens/app/Settings/Settings.tsx
+++ src/screens/app/Settings/Settings.tsx
@@ -1,17 +1,51 @@
+import {FlatList} from 'react-native';
+
 import {useAuthSignOut} from '@domain';
 
-import {Button, Screen, Text} from '@components';
+import {Button, Screen, Separator, Text} from '@components';
 import {AppScreenProps} from '@routes';
+
+import {MenuItem, MenuItemProps} from './components/MenuItem';
 
 export function Settings({navigation}: AppScreenProps<'Settings'>) {
   const {isLoading, signOut} = useAuthSignOut();
 
+  const items: MenuItemProps[] = [
+    {
+      title: 'Termos de uso',
+      onPress: () => {},
+    },
+    {
+      title: 'Política de privacidade',
+      onPress: () => {},
+    },
+    {
+      title: 'Modo noturno',
+      onPress: () => {},
+    },
+  ];
+
+  function renderItem({item}: {item: MenuItemProps}) {
+    return <MenuItem {...item} />;
+  }
+
   return (
-    <Screen canGoBack title="Configurações">
-      <Text preset="headingLarge" mb="s10">
-        Settings
-      </Text>
-      <Button title="Sair da conta" loading={isLoading} onPress={signOut} />
+    <Screen canGoBack title="Configurações" flex={1}>
+      <FlatList
+        data={items}
+        renderItem={renderItem}
+        keyExtractor={item => item.title}
+        ItemSeparatorComponent={Separator}
+        bounces={false}
+        ListFooterComponent={
+          <Button
+            mt="s48"
+            title="Sair da conta"
+            loading={isLoading}
+            onPress={signOut}
+          />
+        }
+      />
     </Screen>
   );
 }
---
+++ src/screens/app/Settings/components/MenuItem.tsx
@@ -0,0 +1,20 @@
+import {Icon, Text, TouchableOpacityBox} from '@components';
+
+export type MenuItemProps = {
+  title: string;
+  onPress: () => void;
+};
+
+export function MenuItem({title, onPress}: MenuItemProps) {
+  return (
+    <TouchableOpacityBox
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      paddingVertical="s16"
+      onPress={onPress}>
+      <Text>{title}</Text>
+      <Icon name="chevronRight" />
+    </TouchableOpacityBox>
+  );
+}
