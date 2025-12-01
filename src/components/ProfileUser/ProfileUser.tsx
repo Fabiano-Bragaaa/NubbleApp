e9276cf551c521da7904ea72b0fc4d5a7718d357
fabiano-bragaaa <fabianobragadev@gmail.com>
2025-12-01 16:50:46 -0300

    Navigation - Lógica com hook customizado

--- src/components/ProfileUser/ProfileUser.tsx
+++ src/components/ProfileUser/ProfileUser.tsx
@@ -4,7 +4,7 @@ import {GestureResponderEvent} from 'react-native';
 import {User} from '@domain';
 import {useNavigation} from '@react-navigation/native';
 
-import {Box, PressableBoxProps} from '../../components/Box/Box';
+import {Box, PressableBox, PressableBoxProps} from '../../components/Box/Box';
 import {
   ProfileAvatar,
   ProfileAvatarProps,
@@ -15,19 +15,32 @@ type ProfileUserProps = {
   user: Pick<User, 'id' | 'profileUrl' | 'username'>;
   avatarProps?: Omit<Partial<ProfileAvatarProps>, 'imageUrl'>;
   rightComponent?: ReactNode;
-};
+} & PressableBoxProps;
 
 export function ProfileUser({
   user,
+  onPress,
   avatarProps,
   rightComponent,
+  ...presableBoxProps
 }: ProfileUserProps) {
+  const {navigate} = useNavigation();
+
+  function handleOnPress(event: GestureResponderEvent) {
+    if (onPress) {
+      onPress(event);
+    }
+    navigate('Profile', {userId: user.id});
+  }
+
   return (
-    <Box
+    <PressableBox
+      onPress={handleOnPress}
       flexDirection="row"
       alignItems="center"
       justifyContent="space-between"
-      mb="s16">
+      mb="s16"
+      {...presableBoxProps}>
       <Box flexDirection="row" alignItems="center">
         <ProfileAvatar
           {...avatarProps}
@@ -39,6 +52,6 @@ export function ProfileUser({
         </Text>
       </Box>
       {rightComponent && rightComponent}
-    </Box>
+    </PressableBox>
   );
 }
